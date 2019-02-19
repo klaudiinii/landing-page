87a7428b8e57c10df34a3f09d3d5913caf79ca05
klaudiinii <romanowska.klaudia93@gmail.com>
2019-02-19 19:45:20 +0100

    adding-toggle-navbar

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-document.getElementById('btn-click').addEventListener('click',
-function() {
-    document.querySelector('.modal-section').style.display = 'block';
-})
+document.getElementById("btn-click").addEventListener("click", function() {
+  document.querySelector(".modal-section").style.display = "block";
+});
 
-document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.modal-section').style.display = 'none';
-})
+document.querySelector(".close").addEventListener("click", function() {
+  document.querySelector(".modal-section").style.display = "none";
+});
+
+$(document).ready(function() {
+  $(".toggle-menu").click(function() {
+    $(".main-nav").toggleClass("toggle-active");
+  });
+});
