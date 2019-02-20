bf85b616d51ebca96d0195b6aa4ce7692c6fd776
klaudiinii <romanowska.klaudia93@gmail.com>
2019-02-20 15:07:17 +0100

    with fetch API

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-document.getElementById("btn-click").addEventListener("click", function() {
+// Show and hide popup
+
+document.getElementById("btn-click").addEventListener("click", () => {
   document.querySelector(".modal-section").style.display = "block";
 });
 
-document.querySelector(".close").addEventListener("click", function() {
+document.querySelector(".close").addEventListener("click", () => {
   document.querySelector(".modal-section").style.display = "none";
 });
+
+// Show and hide popup STOP
+
+// Activate toggle menu
 
 $(document).ready(function() {
   $(".toggle-menu").click(function() {
@@ -12,3 +18,30 @@ $(document).ready(function() {
   });
 });
 
+// Activate toggle menu STOP
+
+// Check form
+
+document.getElementById("login-form").addEventListener("submit", e => {
+  e.preventDefault();
+
+  let login = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  fetch("https://recruitment-api.pyt1.stg.jmr.pl/login", {
+    method: "POST",
+    body: JSON.stringify({
+      login: login,
+      password: password
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then(response => response.json())
+    .then(json => {
+      document.getElementById("response").innerHTML = json.message;
+    });
+});
+
+// Check form STOP
